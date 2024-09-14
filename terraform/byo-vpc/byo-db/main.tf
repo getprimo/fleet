30b198dc516b0e4f051b897f627d7f9d11463dc4
Alexis Bel <alexis.bel96@gmail.com>
2024-09-14 19:24:18 +0200

    fix

--- terraform/byo-vpc/byo-db/main.tf
+++ terraform/byo-vpc/byo-db/main.tf
@@ -5,7 +5,7 @@ module "ecs" {
     loadbalancer = {
       arn = module.alb.target_group_arns[0]
     }
-    extra_load_balancers = var.alb_config == null ? [] : [
+    extra_load_balancers = var.alb_name == null ? [] : [
       {
         target_group_arn = aws_lb_target_group.fleet[0].arn
         container_name   = "fleet"
@@ -32,18 +32,18 @@ module "cluster" {
 }
 
 data "aws_lb" "fleet" {
-  count = var.alb_config == null ? 0 : 1
+  count = var.alb_name == null ? 0 : 1
   name  = var.alb_name
 }
 
 data "aws_lb_listener" "https" {
-  count             = var.alb_config == null ? 0 : 1
+  count             = var.alb_name == null ? 0 : 1
   load_balancer_arn = data.aws_lb.fleet[0].arn
   port              = 443
 }
 
 resource "aws_lb_target_group" "fleet" {
-  count    = var.alb_config == null ? 0 : 1
+  count    = var.alb_name == null ? 0 : 1
   name     = substr("tg-${var.ecs_cluster.cluster_name}", 0, 32)
   port     = 80
   protocol = "HTTP"
@@ -61,7 +61,7 @@ resource "aws_lb_target_group" "fleet" {
 }
 
 resource "aws_lb_listener_rule" "fleet" {
-  count        = var.alb_config == null ? 0 : 1
+  count        = var.alb_name == null ? 0 : 1
   listener_arn = data.aws_lb_listener.https[0].arn
   priority     = 100
 
