09371f2092e8f0043ede6eb6267d67dba97d2c18
Alexis Bel <alexis.bel96@gmail.com>
2024-10-22 23:10:56 +0200

    update fleet iam role

--- terraform/byo-vpc/byo-db/byo-ecs/iam.tf
+++ terraform/byo-vpc/byo-db/byo-ecs/iam.tf
@@ -12,11 +12,25 @@ data "aws_iam_policy_document" "fleet" {
     actions = [
       "s3:GetObject",
       "s3:PutObject",
-      "s3:DeleteObject"
+      "s3:DeleteObject",
+      "s3:ListMultipartUploadParts"
     ]
 
     resources = [
       "arn:aws:s3:::${var.s3_bucket_config.name}/${var.s3_bucket_config.software_path}/*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:ListBucket",
+      "s3:GetBucketLocation"
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.s3_bucket_config.name}"
     ]
   }
 
