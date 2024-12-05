be8f60b73c19a1b1a305745136323d37bc8246ef
Alexis Bel <alexis.bel96@gmail.com>
2024-12-05 17:54:20 +0100

    Update fleet SG so twingate connector can connect to redis

--- terraform/byo-vpc/byo-db/byo-ecs/main.tf
+++ terraform/byo-vpc/byo-db/byo-ecs/main.tf
@@ -272,7 +272,7 @@ resource "aws_security_group" "main" {
       from_port       = 6379
       to_port         = 6379
       protocol        = "TCP"
-      security_groups = [data.aws_security_group.twingate_connector[0]]
+      security_groups = [data.aws_security_group.twingate_connector[0].id]
     }
   }
 }
