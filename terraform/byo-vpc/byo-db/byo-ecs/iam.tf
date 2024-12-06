534fcbb50290727aaa0c6b750bba2d917fff3543
Alexis Bel <alexis.bel96@gmail.com>
2024-12-06 20:55:14 +0100

    Fix iam role

--- terraform/byo-vpc/byo-db/byo-ecs/iam.tf
+++ terraform/byo-vpc/byo-db/byo-ecs/iam.tf
@@ -45,7 +45,7 @@ data "aws_iam_policy_document" "fleet" {
     ]
 
     resources = [
-      "arn:aws:s3:::${var.s3_bucket_config.name}/${var.s3_bucket_config.software_path}/*"
+      "*"
     ]
   }
 
